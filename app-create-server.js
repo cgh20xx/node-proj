ef274a8dd103bfddb8d384f9694891d982abe37e
Hank <cgh20xx@gmail.com>
2022-03-19 13:53:06 +0800

    create http server

---
+++ app-create-server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http
+  .createServer(function (request, response) {
+    console.log(request.url);
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hello');
+    response.end();
+  })
+  .listen(8080);
