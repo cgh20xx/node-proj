56d64d5130a86a819630f65ce99b5dc4276f701f
Hank <cgh20xx@gmail.com>
2022-03-21 01:46:36 +0800

    安裝 nodemon

--- app-create-server.js
+++ app-create-server.js
@@ -3,7 +3,7 @@ http
   .createServer(function (request, response) {
     console.log(request.url);
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello');
+    response.write('Hello hank');
     response.end();
   })
   .listen(8080);
---
+++ app-nodemon.js
@@ -0,0 +1,5 @@
+// nodemon 為開發時自動偵測js改動而自動重啟的套件
+// npm install nodemon -g 全域安裝
+// npm install nodemon --save-dev 安裝在專案
+// 注意:安裝在專案裡需在 package.js 寫 scripts "nodemon": "nodemon" 再執行 npm run nodemon xx.js 才可正確執行)
+console.log('test2');
